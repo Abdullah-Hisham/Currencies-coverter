e7de037b4dbb6b5d3cd1cefbeb43aa22ae8b4597
Abdullah_Hisham <abdulahhisham15@gmail.com>
2024-02-15 11:40:36 +0200

    first

---
+++ backend/converter/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Currency(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    rate = models.FloatField()
+
+
+class UpdateLog(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+class Trysum(models.Model):
+    num = models.IntegerField(default=1)
